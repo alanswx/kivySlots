d8375bb4e0026b7cabab783b7b37a6f7d49870de
Scott Hassan <hassan@willowmail.com>
2017-10-13 11:14:21 -0700

    fixed scoring

--- main.py
+++ main.py
@@ -38,23 +38,26 @@ class Strip(Rectangle):
     self.texture = img.texture
     self.texture.wrap = 'repeat'
     
-  def add_background_uv(self, canvas, val):
-    u = 0
-    v = (self.tex_coords[1] - val)
-    w = 1
-    h = -.7
-    self.tex_coords = [u, v, u+w, v, u+w, v+h, u, v+h]
+  def add_uv(self, canvas, val):
+    self.set_uv(canvas, self.tex_coords[1] - val)
 
-  def set_background_uv(self, canvas, val):
+  def set_uv(self, canvas, val):
     u = 0
-    v = (val+.1)
+    v = val
     w = 1
     h = -.7
     self.tex_coords = [u, v, u+w, v, u+w, v+h, u, v+h]
 
   def strip_pos(self):
-    return round(self.tex_coords[1]*6) % 6
-  
+    return int((1.10-self.tex_coords[1]) / .165) % 6
+
+  def slot_to_uv(self, slot):
+    return 1.10 - (slot * .165)
+
+  def get_uv(self):
+    return (self.tex_coords[0], self.tex_coords[1])
+
+##  [1.100, .942, .759, .598, .444, .273]
 
 class Slots(Widget):
     sw_seconds=0
@@ -63,17 +66,16 @@ class Slots(Widget):
     jackpot=0
 
     def __init__(self, **kwargs):
-        super(Slots, self).__init__(**kwargs)
+      super(Slots, self).__init__(**kwargs)
 
-        self.strips = []
-        with self.canvas:
-
-          for n in range(3):
-            strip = Strip("stripbig%d" % (n+1))
-            strip.set_background_uv(self, 0)
-            self.strips.append(strip)
-          Color(1,0,0)
-          self.payline = Rectangle()
+      self.strips = []
+      with self.canvas:
+        for n in range(3):
+          strip = Strip("stripbig%d" % (n+1))
+          strip.set_uv(self, strip.slot_to_uv(0))
+          self.strips.append(strip)
+        Color(1,0,0)
+        self.payline = Rectangle()
 
     def on_size(self, *args):
       cx = self.size[0]/2
@@ -97,33 +99,38 @@ class Slots(Widget):
             snd_bump.play()
             self.sw_seconds=0
             self.jackpot=0
+        if self.state=='key':
+            self.state ='STATE_SPINNING'
+          
  
     def update(self, nap):
-        if self.state =='idle':
-            self.state='idle'
-            #
-        elif self.state =='STATE_SPINNING':
-            self.sw_seconds += nap
+      if self.state =='idle':
+        self.state='idle'
+      elif self.state =='STATE_SPINNING':
+        self.sw_seconds += nap
 
-            for n in range(self.stopped, len(self.strips)):
-              v = 1.0 + (n*.1)
-              self.strips[n].add_background_uv(self, v * nap)
+        for n in range(self.stopped, len(self.strips)):
+          v = .8 + (n*.1)
+          self.strips[n].add_uv(self, v * nap)
 
-            # check time then switch to next state
-            #print(self.sw_seconds)
-            if self.sw_seconds > self.first_stop_length+self.stopped +random.uniform(0,0.8):  # snap to next "unit"
-              slotnum = round(self.strips[self.stopped].strip_pos())
-              slotnum=slotnum+1
-              self.strips[self.stopped].set_background_uv(self, slotnum/6)
-              if slotnum==2:
-                 print('winner on %d' % (self.stopped+1))
-                 self.jackpot=self.jackpot+1
-              self.stopped += 1
-              if self.stopped >= len(self.strips):
-                self.state = "FINAL"
-        elif self.state =='FINAL':
-          print('total jackpot:',self.jackpot)
-          self.state='idle'
+        #print("{} {}".format(self.strips[0].strip_pos(), self.strips[0].get_uv()[1]))
+
+        #self.state = "key"
+
+        # check time then switch to next state
+        if self.sw_seconds > self.first_stop_length+self.stopped +random.uniform(0,0.8):  # snap to next "unit"
+          slotnum = self.strips[self.stopped].strip_pos()
+          self.strips[self.stopped].set_uv(self, self.strips[self.stopped].slot_to_uv(slotnum))
+          if slotnum==5:
+             print('winner on {}'.format(self.stopped+1))
+             self.jackpot=self.jackpot+1
+          self.stopped += 1
+          if self.stopped >= len(self.strips):
+            self.state = "FINAL"
+
+      elif self.state =='FINAL':
+        print('total jackpot:',self.jackpot)
+        self.state='idle'
 
 class Slot(App):
     playing = False
